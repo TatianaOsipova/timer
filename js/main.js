315771af0ac30a73939faa1f4e2910b1c5ce585a
Tatiana Osipova <otv120780@gmail.com>
2023-04-04 17:29:21 -0400

    feat: change title to English

--- js/main.js
+++ js/main.js
@@ -11,6 +11,8 @@ const daysLeft = Math.floor(diff / 1000 / 60 / 60 / 24);
 const hoursLeft = Math.floor(diff / 1000 / 60 / 60) % 24;
 // Минут всего, далее остаток от преобразования в часы, минут осталось
 const minutesLeft = Math.floor(diff / 1000 / 60) % 60;
+//  Секунд всего, далее остаток от преобразования в минуты,  секунд осталось
+const secondsLeft = Math.floor(diff / 1000) % 60;
 
 
 
