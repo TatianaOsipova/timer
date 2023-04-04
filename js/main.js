71a14d9d338b7a2ae9447e3548cdc1756c19d283
Tatiana Osipova <otv120780@gmail.com>
2023-04-04 17:34:05 -0400

    feat: add comment to my actions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
+// Определяем действующие элементы на странице
+const year = document.querySelector('#year');
+
+
+
 // Делаем расчеты
 const currentYear = new Date().getFullYear();
 const nextYear = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+// Устанавливаем год на страницу
 
 const currentTime = new Date();
 const diff = nextYear - currentTime;
@@ -11,8 +18,10 @@ const daysLeft = Math.floor(diff / 1000 / 60 / 60 / 24);
 const hoursLeft = Math.floor(diff / 1000 / 60 / 60) % 24;
 // Минут всего, далее остаток от преобразования в часы, минут осталось
 const minutesLeft = Math.floor(diff / 1000 / 60) % 60;
-//  Секунд всего, далее остаток от преобразования в минуты,  секунд осталось
+// Секунд всего, далее остаток от преобразования в минуты,  секунд осталось
 const secondsLeft = Math.floor(diff / 1000) % 60;
+
+
 
 
 
