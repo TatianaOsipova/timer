650e960563d6871ec580ea8dc49f10c17b57ef30
Tatiana Osipova <otv120780@gmail.com>
2023-04-04 17:44:04 -0400

    feat: displaying data on the screen

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 // Определяем действующие элементы на странице
 const year = document.querySelector('#year');
-
-
+const days = document.querySelector('#days');
+const hours = document.querySelector('#hours');
+const minutes = document.querySelector('#minutes');
+const seconds = document.querySelector('#seconds');
 
 // Делаем расчеты
 const currentYear = new Date().getFullYear();
 const nextYear = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
 // Устанавливаем год на страницу
+year.textContent = currentYear + 1;
 
 const currentTime = new Date();
 const diff = nextYear - currentTime;
@@ -20,6 +23,12 @@ const hoursLeft = Math.floor(diff / 1000 / 60 / 60) % 24;
 const minutesLeft = Math.floor(diff / 1000 / 60) % 60;
 // Секунд всего, далее остаток от преобразования в минуты,  секунд осталось
 const secondsLeft = Math.floor(diff / 1000) % 60;
+
+days.textContent = daysLeft;
+hours.textContent = hoursLeft;
+minutes.textContent = minutesLeft;
+seconds.textContent = secondsLeft;
+
 
 
 
